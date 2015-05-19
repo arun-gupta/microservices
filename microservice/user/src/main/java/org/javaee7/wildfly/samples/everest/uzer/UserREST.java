20cbc7c1ae69ee3dc9c55215ea893f99b6a4b6bd
arun-gupta <arun.gupta@gmail.com>
2015-05-19 16:08:32 -0700

    removing redundant imports

--- microservice/user/src/main/java/org/javaee7/wildfly/samples/everest/uzer/UserREST.java
+++ microservice/user/src/main/java/org/javaee7/wildfly/samples/everest/uzer/UserREST.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.json.Json;
 import javax.json.JsonObject;
-import javax.json.JsonReader;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
