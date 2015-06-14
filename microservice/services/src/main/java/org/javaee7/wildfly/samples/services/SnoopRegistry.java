91c519b25d1d4e43c024b7feca8cd7ea061275c3
Ivar Grimstad <ivar.grimstad@gmail.com>
2015-06-14 18:36:14 +0200

    Implemented Snoop integration for Service discovery.
    Fixes arun-gupta/microservices#21

---
+++ microservice/services/src/main/java/org/javaee7/wildfly/samples/services/SnoopRegistry.java
@@ -0,0 +1,20 @@
+package org.javaee7.wildfly.samples.services;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+import javax.inject.Qualifier;
+
+/**
+ * 
+ * @author Ivar Grimstad (ivar.grimstad@gmail.com)
+ */
+@Qualifier
+@Retention(RUNTIME)
+@Target({METHOD, FIELD, PARAMETER, TYPE})
+public @interface SnoopRegistry {
+}
---
+++ microservice/services/src/main/java/org/javaee7/wildfly/samples/services/snoop/SnoopServiceRegistry.java
@@ -0,0 +1,67 @@
+package org.javaee7.wildfly.samples.services.snoop;
+
+import eu.agilejava.snoop.annotation.Snoop;
+import eu.agilejava.snoop.client.SnoopDiscoveryClient;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.client.WebTarget;
+import org.javaee7.wildfly.samples.services.ServiceRegistry;
+import org.javaee7.wildfly.samples.services.SnoopRegistry;
+
+/**
+ *
+ * @author Ivar Grimstad (ivar.grimstad@gmail.com)
+ */
+@SnoopRegistry
+@ApplicationScoped
+public class SnoopServiceRegistry implements ServiceRegistry {
+
+   private static final Logger LOGGER = Logger.getLogger(SnoopServiceRegistry.class.getName());
+   
+   @Inject
+   @Snoop(applicationName = "user")
+   private SnoopDiscoveryClient userService;
+
+   @Inject
+   @Snoop(applicationName = "catalog")
+   private SnoopDiscoveryClient catalogService;
+
+   @Inject
+   @Snoop(applicationName = "order")
+   private SnoopDiscoveryClient orderService;
+
+   private final Map<String, WebTarget> services = new HashMap<>();
+
+   @Override
+   public void registerService(String name, String uri) {
+      // noop, handled by snoop
+   }
+
+   @Override
+   public void unregisterService(String name, String uri) {
+      // noop, handled by snoop
+   }
+
+   @Override
+   public String discoverService(String name) {
+      final String endpoint = Optional.ofNullable(services.get(name))
+              .orElseThrow(RuntimeException::new)
+              .getUri()
+              .toString();
+      
+      LOGGER.config(() -> "Returning service endpoint for '" + name + "': " + endpoint);
+      return endpoint;
+   }
+
+   @PostConstruct
+   public void init() {
+      services.put("user", userService.getServiceRoot());
+      services.put("catalog", catalogService.getServiceRoot());
+      services.put("order", orderService.getServiceRoot());
+   }
+}
